8a68fc301c4dac29d3f62db65a1790134efd0e67
volaya <volayaf@gmail.com>
2015-07-17 12:02:03 +0200

    adapted legend to work with timeline controls

--- webappbuilder/appwriter.py
+++ webappbuilder/appwriter.py
@@ -484,47 +484,62 @@ def writeLegendFiles(appdef, folder):
     if not QDir(legendFolder).exists():
         QDir().mkpath(legendFolder)
     for ilayer, applayer in enumerate(layers):
+        if applayer.timeInfo is not None:
+            continue
         layer = applayer.layer
-        symbols = {}
-        size = 20
-        qsize = QSize(size, size)
-        if layer.type() == layer.VectorLayer:
-            renderer = layer.rendererV2()
-            if isinstance(renderer, QgsSingleSymbolRendererV2):
-                    img = renderer.symbol().asImage(qsize)
-                    symbolPath = os.path.join(legendFolder, "%i_0.png" % (ilayer))
-                    img.save(symbolPath)
-                    symbols[""] = os.path.basename(symbolPath)
-            elif isinstance(renderer, QgsCategorizedSymbolRendererV2):
-                for isymbol, cat in enumerate(renderer.categories()):
-                    img = cat.symbol().asImage(qsize)
-                    symbolPath = os.path.join(legendFolder, "%i_%i.png" % (ilayer, isymbol))
-                    img.save(symbolPath)
-                    symbols[cat.label()] = os.path.basename(symbolPath)
-            elif isinstance(renderer, QgsGraduatedSymbolRendererV2):
-                for ran in renderer.ranges():
-                    img = ran.symbol().asImage(qsize)
-                    symbolPath = os.path.join(legendFolder, "%i_%i.png" % (ilayer, isymbol))
-                    img.save(symbolPath)
-                    symbols["%s-%s" % (ran.lowerValue(), ran.upperValue())] = os.path.basename(symbolPath)
+        symbols = getLegendSymbols(layer, ilayer, legendFolder)
+        if symbols:
             legend[layer.name()] = symbols
-        elif layer.providerType() == "wms":
-            source = layer.source()
-            layerName = re.search(r"layers=(.*?)(?:&|$)", source).groups(0)[0]
-            url = re.search(r"url=(.*?)(?:&|$)", source).groups(0)[0]
-            styles = re.search(r"styles=(.*?)(?:&|$)", source).groups(0)[0]
-            fullUrl = ("%s?LAYER=%s&STYLES=%s&REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=%i&HEIGHT=%i"
-                       % (url, layerName, styles, size, size))
-            response = requests.get(fullUrl, stream=True)
-            symbolPath = os.path.join(legendFolder, "%i_0.png" % ilayer)
-            with open(symbolPath, 'wb') as f:
-                shutil.copyfileobj(response.raw, f)
-            del response
-            symbols[""] = os.path.basename(symbolPath)
-            legend[layer.name()] = symbols
+
+    qgisLayers = {applayer.layer.id(): applayer for applayer in layers}
+    groups = appdef["Groups"]
+    for group, groupLayers in groups.iteritems():
+        hasTimeInfo = any([qgisLayers[lay.id()].timeInfo is not None for lay in groupLayers])
+        if hasTimeInfo:
+            symbols = getLegendSymbols(groupLayers[0], ilayer, legendFolder)
+            if symbols:
+                legend[group] = symbols
 
     with open(os.path.join(legendFolder, "legend.js"), "w") as f:
         f.write("var legendData = %s;" % json.dumps(legend))
+
+def getLegendSymbols(layer, ilayer, legendFolder):
+    size = 20
+    qsize = QSize(size, size)
+    symbols = {}
+    if layer.type() == layer.VectorLayer:
+        renderer = layer.rendererV2()
+        if isinstance(renderer, QgsSingleSymbolRendererV2):
+                img = renderer.symbol().asImage(qsize)
+                symbolPath = os.path.join(legendFolder, "%i_0.png" % (ilayer))
+                img.save(symbolPath)
+                symbols[""] = os.path.basename(symbolPath)
+        elif isinstance(renderer, QgsCategorizedSymbolRendererV2):
+            for isymbol, cat in enumerate(renderer.categories()):
+                img = cat.symbol().asImage(qsize)
+                symbolPath = os.path.join(legendFolder, "%i_%i.png" % (ilayer, isymbol))
+                img.save(symbolPath)
+                symbols[cat.label()] = os.path.basename(symbolPath)
+        elif isinstance(renderer, QgsGraduatedSymbolRendererV2):
+            for ran in renderer.ranges():
+                img = ran.symbol().asImage(qsize)
+                symbolPath = os.path.join(legendFolder, "%i_%i.png" % (ilayer, isymbol))
+                img.save(symbolPath)
+                symbols["%s-%s" % (ran.lowerValue(), ran.upperValue())] = os.path.basename(symbolPath)
+    elif layer.providerType() == "wms":
+        source = layer.source()
+        layerName = re.search(r"layers=(.*?)(?:&|$)", source).groups(0)[0]
+        url = re.search(r"url=(.*?)(?:&|$)", source).groups(0)[0]
+        styles = re.search(r"styles=(.*?)(?:&|$)", source).groups(0)[0]
+        fullUrl = ("%s?LAYER=%s&STYLES=%s&REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=%i&HEIGHT=%i"
+                   % (url, layerName, styles, size, size))
+        response = requests.get(fullUrl, stream=True)
+        symbolPath = os.path.join(legendFolder, "%i_0.png" % ilayer)
+        with open(symbolPath, 'wb') as f:
+            shutil.copyfileobj(response.raw, f)
+        del response
+        symbols[""] = os.path.basename(symbolPath)
+    return symbols
 
 def bounds(useCanvas, layers, crsid):
     if useCanvas:
