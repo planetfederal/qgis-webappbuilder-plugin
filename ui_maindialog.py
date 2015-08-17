89a4599edda39dba5a1e04f1c678e4972e131a9e
volaya <volayaf@gmail.com>
2015-08-17 13:08:39 +0200

    removed unused file

--- ui_maindialog.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ui_maindialog.ui'
-#
-# Created: Fri Aug 14 08:27:30 2015
-#      by: PyQt4 UI code generator 4.11.1
-#
-# WARNING! All changes made in this file will be lost!
-
