036fefdb810d09c8b1a56696059902ceb6728701
volaya <volayaf@gmail.com>
2015-08-20 14:38:08 +0200

    more options for 'add layer' component

--- webappbuilder/widgets/addlayer/addlayer.js
+++ webappbuilder/widgets/addlayer/addlayer.js
@@ -16,7 +16,7 @@ var addLayerFromFile = function(){
         return null;
     }
 
-    var _addLayerFromFile = function(f){
+    var _addLayerFromFile = function(f, color, fillColor){
         if (f) {
             var r = new FileReader();
             r.onload = function(e) {
@@ -31,6 +31,44 @@ var addLayerFromFile = function(){
                         isRemovable: true,
                         isSelectable: true
                     });
+                    var style = function(feature, resolution) {
+                        var selected = lyr.selectedFeatures;
+                        if (selected && selected.indexOf(feature) != -1) {
+                            return [new ol.style.Style({
+                                        fill: new ol.style.Fill({
+                                            color: "rgba(255, 204, 0, 1)",
+                                        }),
+                                        stroke: new ol.style.Stroke({
+                                            color: "rgba(255, 204, 0, 1)",
+                                            width: 2
+                                        }),
+                                        image: new ol.style.Circle({
+                                            radius: 7,
+                                            fill: new ol.style.Fill({
+                                                color: "rgba(255, 204, 0, 1)",
+                                            })
+                                        })
+                                    })];
+                        }
+                        else{
+                            return [new ol.style.Style({
+                                        fill: new ol.style.Fill({
+                                            color: fillColor
+                                        }),
+                                        stroke: new ol.style.Stroke({
+                                            color: color,
+                                            width: 2
+                                        }),
+                                        image: new ol.style.Circle({
+                                            radius: 7,
+                                            fill: new ol.style.Fill({
+                                                color: color
+                                            })
+                                        })
+                                    })];
+                        }
+                    };
+                    lyr.setStyle(style);
                     map.addLayer(lyr);
                 }
                 else{
@@ -44,15 +82,57 @@ var addLayerFromFile = function(){
         }
     };
 
-    var input = document.createElement('input');
-    input.type = "file";
-    input.accept=".geojson, .gpx, .kml"
-    $(input).on("change", function(){
-        var filename = input.files[0];
-        busyProcess(function(){_addLayerFromFile(filename)}, 500);
-        ;
+    var html = '<div class="row">  ' +
+            '<div class="col-md-12"> ' +
+            '<form class="form-horizontal"> ' +
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-file-textbox"> File </label>'+
+            '<div class="col-md-8">'  +
+            '<div class="input-group">' +
+            '<input id="new-layer-file-selector" type="file" accept=".geojson, .gpx, .kml" style="display:none">' +
+            '<input id="new-layer-file-textbox" class="form-control" type="text">' +
+            '<div class="input-group-addon">'+
+            '<a href="#" onclick="$(\'input[id=new-layer-file-selector]\').click();">Browse</a>' +
+            '</div></div>' +
+            '</div></div>'+
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-color"> Color </label>'+
+            '<div class="col-md-8"> <input value="#000000" id="new-layer-color" type="text" class="color-picker form-control">' +
+            '</div></div>'+
+            '<div class="form-group"> ' +
+            '<label class="col-md-4 control-label" for="new-layer-fillcolor"> Fill color (polygons only) </label>'+
+            '<div class="col-md-8"> <input value="#0000aa" id="new-layer-fillcolor" type="text" class="color-picker form-control">' +
+            '</div></div>'+
+            '</form></div></div>';
+
+    var dialog = bootbox.dialog({
+        title: "Create empty layer",
+        message: html,
+        buttons: {
+            success: {
+                label: "Ok",
+                className: "btn-success",
+                callback: function () {
+                    var file = $("#new-layer-file-selector").prop("files")[0]
+                    var color = $("#new-layer-color").val().toString();
+                    var fillColor = $("#new-layer-fillcolor").val().toString();
+                    busyProcess(function(){_addLayerFromFile(file, color, fillColor)}, 500);
+                }
+            }
+        },
+        show: false
     });
-    $(input).trigger('click');
+
+    dialog.on('show.bs.modal', function(){
+        $(".color-picker").colorpicker();
+        $('input[id=new-layer-file-selector]').change(function() {
+            $('#new-layer-file-textbox').val($(this).val());
+        });
+    });
+
+    dialog.modal("show");
+
+
 
     return false;
 
