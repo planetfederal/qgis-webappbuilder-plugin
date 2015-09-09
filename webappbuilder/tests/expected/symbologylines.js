3dac03aec4c5e8a6669f89a5dc8ddabb60535735
volaya <volayaf@gmail.com>
2015-09-09 08:44:08 +0200

    replaced incorrect expected outputs for tests

--- webappbuilder/tests/expected/symbologylines.js
+++ webappbuilder/tests/expected/symbologylines.js
@@ -1,125 +1,91 @@
-var categories_lines = {
-    "2": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(84,229,181,1.0)",
-            lineDash: null,
-            width: 0
-        })
-    })],
-    "3": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(59,37,41,1.0)",
-            lineDash: [6],
-            width: 0
-        })
-    })],
-    "4": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(122,245,0,1.0)",
-            lineDash: null,
-            width: 5
-        })
-    }), new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(170,48,0,1.0)",
-            lineDash: [6],
-            width: 5
-        })
-    })],
-    "5": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(143,135,230,1.0)",
-            lineDash: null,
-            width: 7
-        })
-    })],
-    "6": [new ol.style.Style({
-
-    })],
-    "7": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255,255,255,1.0)",
-            lineDash: null,
-            width: 4
-        })
-    }), new ol.style.Style({
-
-    })],
-    "8": [new ol.style.Style({
-
-    })]
-};
-var categoriesSelected_lines = {
-    "2": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 0
-        })
-    })],
-    "3": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: [6],
-            width: 0
-        })
-    })],
-    "4": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 5
-        })
-    }), new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: [6],
-            width: 5
-        })
-    })],
-    "5": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 7
-        })
-    })],
-    "6": [new ol.style.Style({
-
-    })],
-    "7": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 4
-        })
-    }), new ol.style.Style({
-
-    })],
-    "8": [new ol.style.Style({
-
-    })]
-};
-var textStyleCache_lines = {}
-var clusterStyleCache_lines = {}
-var selectedClusterStyleCache_lines = {}
-var style_lines = function(feature, resolution) {
-    var selected = lyr_lines.selectedFeatures;
-
-    if (feature.hide === true) {
-        return null;
-    }
-
-
-    var value = feature.get("n");
-    var style = categories_lines[value];
-    var selectionStyle = categoriesSelected_lines[value];
-    allStyles = [];
-
-    if (selected && selected.indexOf(feature) != -1) {
-        allStyles.push.apply(allStyles, selectionStyle);
-    } else {
-        allStyles.push.apply(allStyles, style);
-    }
-    return allStyles;
-};
+var categories_lines = {"2": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(84,229,181,1.0)", lineDash: null, width: 0})
+                        })
+                        ],
+"3": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(59,37,41,1.0)", lineDash: [6], width: 0})
+                        })
+                        ],
+"4": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(122,245,0,1.0)", lineDash: null, width: 5})
+                        })
+                        ,new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(170,48,0,1.0)", lineDash: [6], width: 5})
+                        })
+                        ],
+"5": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(143,135,230,1.0)", lineDash: null, width: 7})
+                        })
+                        ],
+"6": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"7": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255,255,255,1.0)", lineDash: null, width: 4})
+                        })
+                        ,new ol.style.Style({
+                            
+                        })
+                        ],
+"8": [ new ol.style.Style({
+                            
+                        })
+                        ]};var categoriesSelected_lines = {"2": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0})
+                        })
+                        ],
+"3": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: [6], width: 0})
+                        })
+                        ],
+"4": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 5})
+                        })
+                        ,new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: [6], width: 5})
+                        })
+                        ],
+"5": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 7})
+                        })
+                        ],
+"6": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"7": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 4})
+                        })
+                        ,new ol.style.Style({
+                            
+                        })
+                        ],
+"8": [ new ol.style.Style({
+                            
+                        })
+                        ]};
+                        var textStyleCache_lines={}
+                        var clusterStyleCache_lines={}
+                        var selectedClusterStyleCache_lines={}
+                        var style_lines = function(feature, resolution){
+                        var selected = lyr_lines.selectedFeatures;
+                        
+                        if (feature.hide === true){
+                return null;
+            }
+            
+                        
+                        var value = feature.get("n");
+                        var style = categories_lines[value];
+                        var selectionStyle = categoriesSelected_lines[value];
+                        allStyles = [];
+                        
+                        if (selected && selected.indexOf(feature) != -1){
+                            allStyles.push.apply(allStyles, selectionStyle);
+                        }
+                        else{
+                            allStyles.push.apply(allStyles, style);
+                        }
+                        return allStyles;
+                    };
--- webappbuilder/tests/expected/symbologypolygons.js
+++ webappbuilder/tests/expected/symbologypolygons.js
@@ -1,181 +1,129 @@
-var categories_polygons = {
-    "2": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(0,0,0,1.0)",
-            lineDash: [3],
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(32,225,68,1.0)"
-        })
-    })],
-    "3": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(0,0,0,1.0)",
-            lineDash: null,
-            width: 3
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(116,229,72,1.0)"
-        })
-    })],
-    "4": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(0,0,0,1.0)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(0,0,0,0.0)"
-        })
-    })],
-    "5": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(0,0,0,1.0)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(217,116,133,1.0)"
-        })
-    }), new ol.style.Style({
-
-    })],
-    "6": [new ol.style.Style({
-
-    })],
-    "7": [new ol.style.Style({
-
-    })],
-    "8": [new ol.style.Style({
-
-    })],
-    "9": [new ol.style.Style({
-
-    })],
-    "10": [new ol.style.Style({
-
-    })],
-    "11": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(244,0,0,1.0)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(110,181,234,1.0)"
-        })
-    })],
-    "": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(0,0,0,1.0)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(29,37,130,1.0)"
-        })
-    })]
-};
-var categoriesSelected_polygons = {
-    "2": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: [3],
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    })],
-    "3": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 3
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    })],
-    "4": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    })],
-    "5": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    }), new ol.style.Style({
-
-    })],
-    "6": [new ol.style.Style({
-
-    })],
-    "7": [new ol.style.Style({
-
-    })],
-    "8": [new ol.style.Style({
-
-    })],
-    "9": [new ol.style.Style({
-
-    })],
-    "10": [new ol.style.Style({
-
-    })],
-    "11": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    })],
-    "": [new ol.style.Style({
-        stroke: new ol.style.Stroke({
-            color: "rgba(255, 204, 0, 1)",
-            lineDash: null,
-            width: 0
-        }),
-        fill: new ol.style.Fill({
-            color: "rgba(255, 204, 0, 1)"
-        })
-    })]
-};
-var textStyleCache_polygons = {}
-var clusterStyleCache_polygons = {}
-var selectedClusterStyleCache_polygons = {}
-var style_polygons = function(feature, resolution) {
-    var selected = lyr_polygons.selectedFeatures;
-
-    if (feature.hide === true) {
-        return null;
-    }
-
-
-    var value = feature.get("n");
-    var style = categories_polygons[value];
-    var selectionStyle = categoriesSelected_polygons[value];
-    allStyles = [];
-
-    if (selected && selected.indexOf(feature) != -1) {
-        allStyles.push.apply(allStyles, selectionStyle);
-    } else {
-        allStyles.push.apply(allStyles, style);
-    }
-    return allStyles;
-};
+var categories_polygons = {"2": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: [6], width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(32,225,68,1.0)"})
+                        })
+                        ],
+"3": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 3}),
+                        fill: new ol.style.Fill({color: "rgba(116,229,72,1.0)"})
+                        })
+                        ],
+"4": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(0,0,0,0.0)"})
+                        })
+                        ],
+"5": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(217,116,133,1.0)"})
+                        })
+                        ,new ol.style.Style({
+                            
+                        })
+                        ],
+"6": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"7": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"8": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"9": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"10": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"11": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(244,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(110,181,234,1.0)"})
+                        })
+                        ],
+"": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(0,0,0,1.0)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(29,37,130,1.0)"})
+                        })
+                        ]};var categoriesSelected_polygons = {"2": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: [6], width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ],
+"3": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 3}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ],
+"4": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ],
+"5": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ,new ol.style.Style({
+                            
+                        })
+                        ],
+"6": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"7": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"8": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"9": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"10": [ new ol.style.Style({
+                            
+                        })
+                        ],
+"11": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ],
+"": [ new ol.style.Style({
+                            stroke: new ol.style.Stroke({color: "rgba(255, 204, 0, 1)", lineDash: null, width: 0}),
+                        fill: new ol.style.Fill({color: "rgba(255, 204, 0, 1)"})
+                        })
+                        ]};
+                        var textStyleCache_polygons={}
+                        var clusterStyleCache_polygons={}
+                        var selectedClusterStyleCache_polygons={}
+                        var style_polygons = function(feature, resolution){
+                        var selected = lyr_polygons.selectedFeatures;
+                        
+                        if (feature.hide === true){
+                return null;
+            }
+            
+                        
+                        var value = feature.get("n");
+                        var style = categories_polygons[value];
+                        var selectionStyle = categoriesSelected_polygons[value];
+                        allStyles = [];
+                        
+                        if (selected && selected.indexOf(feature) != -1){
+                            allStyles.push.apply(allStyles, selectionStyle);
+                        }
+                        else{
+                            allStyles.push.apply(allStyles, style);
+                        }
+                        return allStyles;
+                    };
