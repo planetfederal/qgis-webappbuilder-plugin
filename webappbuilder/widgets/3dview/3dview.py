7617edf2027b184a2b627a898c3a6114c43750f9
volaya <volayaf@gmail.com>
2015-10-12 07:29:10 +0200

    fixed Globe component

--- webappbuilder/widgets/3dview/3dview.py
+++ webappbuilder/widgets/3dview/3dview.py
@@ -6,22 +6,7 @@ import shutil
 class ThreeDView(WebAppWidget):
 
     def write(self, appdef, folder, app, progress):
-        self.addScript("cesium-control.js", folder, app)
-        self.addScript("Cesium.js", folder, app)
-        self.addScript("ol3cesium.js", folder, app)
-        self.addCss("3dview.css", folder, app)
-        self.copyToResources("exports.js", folder)
-        self.copyToResources("ol3cesium.js.map", folder)
-        dst = os.path.join(folder, "resources", "Assets")
-        resourcesFolder = os.path.join(os.path.dirname(__file__), "Assets")
-        shutil.copytree(resourcesFolder, dst)
-        app.postmap.append('''var ol3d = new olcs.OLCesium({map: map});
-                    var scene = ol3d.getCesiumScene();
-                    var terrainProvider = new Cesium.CesiumTerrainProvider({
-                        url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
-                    });
-                    scene.terrainProvider = terrainProvider;
-                    map.addControl(new ol.control.CesiumControl(ol3d))''')
+        app.controls.append("<div id='home-button' className='ol-unselectable ol-control'><Globe map={map} /></div>")
 
 
     def icon(self):
