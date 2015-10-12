8ecc8a473c3546aa3db57e9cb4a729ab48249bf6
volaya <volayaf@gmail.com>
2015-10-12 09:51:17 +0200

    added edit panel

--- webappbuilder/widgets/edit/edit.py
+++ webappbuilder/widgets/edit/edit.py
@@ -1,45 +1,13 @@
 from webappbuilder.webbappwidget import WebAppWidget
 import os
 from PyQt4.QtGui import QIcon
-import shutil
 
 class Edit(WebAppWidget):
 
     def write(self, appdef, folder, app, progress):
-        self.addScript("edit.js", folder, app)
-        self.addCss("edit.css", folder, app)
-        self.addScript("bootbox.min.js", folder, app)
-        app.scripts.append('<script src="./resources/colorpicker/js/bootstrap-colorpicker.min.js"></script>')
-        app.scripts.append('<link href="./resources/colorpicker/css/bootstrap-colorpicker.min.css" rel="stylesheet" type="text/css"/>')
-        dst = os.path.join(folder, "resources", "colorpicker")
-        colorpickerFolder = os.path.join(os.path.dirname(__file__), "colorpicker")
-        if os.path.exists(dst):
-            shutil.rmtree(dst)
-        shutil.copytree(colorpickerFolder, dst)
-        app.tools.append('<li><a onclick="showEditPanel()" href="#"><i class="glyphicon glyphicon-pencil"></i>Edit</a></li>')
-        app.mappanels.append('''<div class="edit-tool-panel" id="edit-tool-panel">
-                                  <form class="form-inline">
-                                   <div class="input-group">
-                                    <div class="input-group-addon">
-                                     Layer
-                                    </div>
-                                    <select class="form-control" id="edit-layer">
-                                    </select>
-                                    <div class="input-group-addon">
-                                     <a href="#" id="btn-add-empty-layer">
-                                      <i class="glyphicon glyphicon-plus">
-                                      </i>
-                                     </a>
-                                    </div>
-                                   </div>
-                                   <a class="btn btn-primary" href="#" id="btn-edit-tool">
-                                    Enable edit mode
-                                   </a>
-                                   <a class="btn btn-default" href="#" id="btn-close-edit">
-                                   Close
-                                  </a>
-                                  </form>
-                                </div>''')
+        app.tools.append("<ul className='pull-right' id='toolbar-edit'><UI.DefaultButton "
+        + "onClick={this._toggleEdit.bind(this)}><Icon.Icon name='pencil' /> Edit</UI.DefaultButton></ul>")
+        app.mappanels.append("<div id='edit-tool-panel'><Edit toggleGroup='navigation' map={map} /></div>")
 
     '''def icon(self):
         return QIcon(os.path.join(os.path.dirname(__file__), "edit.png"))'''
