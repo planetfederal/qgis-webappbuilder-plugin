7c21fddff36770fb3b58ae92d481edc7666fd3f3
volaya <volayaf@gmail.com>
2015-10-13 11:43:34 +0200

    added button to disable selection tools

--- webappbuilder/widgets/selectiontools/selectiontools.py
+++ webappbuilder/widgets/selectiontools/selectiontools.py
@@ -6,7 +6,7 @@ class SelectionTools(WebAppWidget):
 
     def write(self, appdef, folder, app, progress):
         app.tools.append("<ul className='pull-right' id='toolbar-select'><Select toggleGroup='navigation' map={map}/></ul>")
-
+        app.tools.append("<ul className='pull-right' id='toolbar-navigation'><BUTTON.DefaultButton title='Switch to map navigation (pan and zoom)' onClick={this._navigationFunc}>Navigation</BUTTON.DefaultButton></ul>")
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(__file__), "selection-tool.png"))
 
