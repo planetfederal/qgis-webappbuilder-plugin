cd2884cd5a493caad1a9fa6ab6344845c2413283
volaya <volayaf@gmail.com>
2015-10-19 16:09:49 +0200

    implement method to detect sdk and node

--- webappbuilder/sdkutils.py
+++ webappbuilder/sdkutils.py
@@ -1,2 +1,13 @@
+import os
+import execjs
+
 def isSdkInstalled():
-    return False
+    sdkPath = os.path.join(os.path.dirname(__file__), "websdk")
+    if not os.path.exists(sdkPath):
+        return False
+    try:
+        node = execjs.get("Node")
+        return node.is_available()
+    except execjs.RuntimeUnavailable:
+        return False
+
