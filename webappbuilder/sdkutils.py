c4d3500feadcc11526080dc4a03d6fe4bde9ebaa
volaya <volayaf@gmail.com>
2015-10-29 08:03:11 +0100

    changed criteria for finding out if sdk is installed

--- webappbuilder/sdkutils.py
+++ webappbuilder/sdkutils.py
@@ -1,13 +1,6 @@
 import os
-import execjs
 
 def isSdkInstalled():
-    sdkPath = os.path.join(os.path.dirname(__file__), "websdk")
-    if not os.path.exists(sdkPath):
-        return False
-    try:
-        node = execjs.get("Node")
-        return node.is_available()
-    except execjs.RuntimeUnavailable:
-        return False
+    sdkPath = os.path.join(os.path.dirname(__file__), "websdk", "node_modules")
+    return os.path.exists(sdkPath)
 
