9b1257a5e896a7550786769814c6599760216f13
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-06-07 15:25:10 +0200

    Changes to the tabbed theme

--- webappbuilder/widgets/homebutton/homebutton.py
+++ webappbuilder/widgets/homebutton/homebutton.py
@@ -5,7 +5,7 @@ from PyQt4.QtGui import QIcon
 class HomeButton(WebAppWidget):
 
     def write(self, appdef, folder, app, progress):
-        app.panels.append('''React.createElement("div", {id:'home-button', className:'ol-unselectable ol-control'},
+        app.panels.append('''React.createElement("div", {id:'home-button'},
                                     React.createElement(HomeButton, {map:map})
                                   )''')
 
