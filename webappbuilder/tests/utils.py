ce85d0f4dffc0dd347f856fafdaed994926d1ce4
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-06-08 15:50:15 +0200

    Fixed WFS test in layerstest

--- webappbuilder/tests/utils.py
+++ webappbuilder/tests/utils.py
@@ -4,6 +4,7 @@
 # This code is licensed under the GPL 2.0 license.
 #
 import os
+import re
 import codecs
 from qgis.utils import iface
 from qgis.core import *
@@ -51,13 +52,24 @@ def createAppFromTestAppdef(appdefName):
     writeWebApp(appdef, folder, True, True, SilentProgress())
     return folder
 
+def ignoreLayerID(text):
+    """
+    Normalize the digit part of an id identifier:
+
+    id: "osm_placenames_large20160602142332803"
+    becomes:
+    id: "osm_placenames_large0000"
+￼
+    """
+    return re.sub(r'(id: "[^\d]+)(\d+)"', r'\g<1>0000', text)
+
 def compareWithExpectedOutputFile(file1, file2):
     with codecs.open(file1, encoding="utf-8") as f:
         content = f.read()
     filename2 = os.path.join(os.path.dirname(__file__), "expected", file2)
     with codecs.open(filename2, encoding="utf-8") as f:
         content2 = f.read()
-    return content2 in content
+    return ignoreLayerID(content2) in ignoreLayerID(content)
 
 def checkTextInFile(filename, text):
     with codecs.open(filename, encoding="utf-8") as f:
