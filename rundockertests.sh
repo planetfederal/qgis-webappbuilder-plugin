f6645a36bd7089b980987c5a086ceab0671ee5f2
Alessandro Pasotti <apasotti@boundlessgeo.com>
2016-06-14 11:40:43 +0200

    Added the script to run the tests locally in the docker

---
+++ rundockertests.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Run the tests locally using the qgis testing environment docker
+
+PLUGIN_NAME="webappbuilder"
+
+docker rm -f qgis-testing-environment
+
+#docker pull elpaso/qgis-testing-environment:latest
+docker tag elpaso/qgis-testing-environment:latest qgis-testing-environment
+
+docker run -d  --name qgis-testing-environment  -e DISPLAY=:99 -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/tests_directory elpaso/qgis-testing-environment:latest
+
+
+docker exec -it qgis-testing-environment sh -c "qgis_setup.sh $PLUGIN_NAME"
+
+# SSH key
+docker exec -it qgis-testing-environment sh -c "mkdir -p /root/.ssh"
+docker exec -it qgis-testing-environment sh -c "printf 'Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null\n' > /root/.ssh/config"
+docker exec -it qgis-testing-environment sh -c "cp /tests_directory/travis_setup/id_rsa /root/.ssh/id_rsa"
+docker exec -it qgis-testing-environment sh -c "chmod 600 /root/.ssh/id_rsa"
+
+# Install the plugin
+docker exec -it qgis-testing-environment sh -c "pip install paver"
+docker exec -it qgis-testing-environment sh -c "cd /tests_directory && paver setup"
+docker exec -it qgis-testing-environment sh -c "ln -s /tests_directory/$PLUGIN_NAME /root/.qgis2/python/plugins/$PLUGIN_NAME"
+
+# run the tests
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.layerstest.run_tests"
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.symbologytest.run_tests"
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.widgetstest.run_tests"
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.settingstest.run_tests"
+docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.appdefvaliditytest.run_tests"
