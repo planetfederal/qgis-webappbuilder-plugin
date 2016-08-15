c22eecd9e347e5e1c600f56661b33722ba6693ef
Alexander Bruy <alexander.bruy@gmail.com>
2016-08-15 15:42:06 +0300

    attribution should be string not array (fix #165)

--- webappbuilder/tests/expected/layers_multiplebaselayers.js
+++ webappbuilder/tests/expected/layers_multiplebaselayers.js
@@ -3,7 +3,7 @@ var baseLayers = [new ol.layer.Tile({
     title: 'CartoDB light',
     source: new ol.source.XYZ({
         url: 'http://s.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-        attributions: [new ol.Attribution({ html: ['&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>']
+        attributions: [new ol.Attribution({ html: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
         })]
     }),
     projection: 'EPSG:3857'
