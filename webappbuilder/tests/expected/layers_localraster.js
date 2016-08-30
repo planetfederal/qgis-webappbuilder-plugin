9140cfebed79e580760b3c984488bfb1e2389a08
Alexander Bruy <alexander.bruy@gmail.com>
2016-08-30 16:06:33 +0300

    more complete test fix

--- webappbuilder/tests/expected/layers_localraster.js
+++ webappbuilder/tests/expected/layers_localraster.js
@@ -1,19 +1,29 @@
-var baseLayers = [];var baseLayersGroup = new ol.layer.Group({showContent: true,'type':
-                    'base-group', 'title': 'Base maps', layers: baseLayers});
-var overlayLayers = [];var overlaysGroup = new ol.layer.Group({showContent: true, 'title': 'Overlays', layers: overlayLayers});
-var lyr_raster = new ol.layer.Image({
+var src_raster = new ol.source.ImageStatic({
+                                url: "./data/raster.png",
+                                projection: "EPSG:3857",
+                                alwaysInRange: true,
+                                imageSize: [91, 91],
+                                imageExtent: [50093.770857, 50094.285871, 1063101.137076, 1068058.094050]
+                          });
+
+                          var raster_raster = new ol.source.Raster({
+                                sources: [src_raster],
+                                operation: function(pixels, data) {
+                                    var pixel = pixels[0];
+                                    if (pixel[0] === 0 && pixel[1] === 0 && pixel[2] === 0) {
+                                        pixel[3] = 0;
+                                    }
+                                    return pixel;
+                                  }
+                          });
+
+                          var lyr_raster = new ol.layer.Image({
                                 opacity: 1.0,
                                  
                                 title: "raster",
                                 id: "raster20150909093752545",
                                 timeInfo: null,
-                                source: new ol.source.ImageStatic({
-                                   url: "./data/raster.png",
-                                    projection: "EPSG:3857",
-                                    alwaysInRange: true,
-                                    imageSize: [91, 91],
-                                    imageExtent: [50093.770857, 50094.285871, 1063101.137076, 1068058.094050]
-                                })
+                                source: raster_raster
                             });
 
 lyr_raster.setVisible(true);
