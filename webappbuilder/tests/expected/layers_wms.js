6f51359f27fb3cd3cdde75e15c95d64fdb3f5f21
Alexander Bruy <alexander.bruy@gmail.com>
2016-11-08 11:08:00 +0200

    use WMS layer from 3rd party (more reliable) server

--- webappbuilder/tests/expected/layers_wms.js
+++ webappbuilder/tests/expected/layers_wms.js
@@ -1,16 +1,13 @@
-var baseLayers = [];var baseLayersGroup = new ol.layer.Group({showContent: true,'type':
-                    'base-group', 'title': 'Base maps', layers: baseLayers});
-var overlayLayers = [];var overlaysGroup = new ol.layer.Group({showContent: true, 'title': 'Overlays', layers: overlayLayers});
 var lyr_wms = new ol.layer.Tile({
                         opacity: 1.0,
                         timeInfo: null,
                          
                         source: new ol.source.TileWMS(({
-                          url: "http://demo.boundlessgeo.com/geoserver/wms",
-                          params: {"LAYERS": "osm:osm" , "TILED": "true", "STYLES": ""},
+                          url: "http://nsidc.org/cgi-bin/atlas_south?",
+                          params: {"LAYERS": "blue_marble_01" , "TILED": "true", "STYLES": ""},
                         })),
                         title: "wms",
-                        id: "osm_osm20150909090852137",
+                        id: "satellite_imagery__January__2004_20161107140352374",
                         popupInfo: "",
                         projection: "EPSG:4326"
                       });
