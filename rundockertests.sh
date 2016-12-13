d1255976b36873fffdde08329d5af3ccaf28dc83
Alexander Bruy <alexander.bruy@gmail.com>
2016-12-13 12:53:29 +0200

    use correct Python and pip executables

--- rundockertests.sh
+++ rundockertests.sh
@@ -11,15 +11,18 @@ PLUGIN_NAME="webappbuilder"
 docker rm -f qgis-testing-environment
 
 # replace master_2 with master or release if you wish to test on those builds
+TARGET_VERSION=master
 
-QGIS_VERSION_TAG=master_2
+if [ $TARGET_VERSION = 'master' ]; then
+    PIP_EXECUTABLE=pip3
+else
+    PIP_EXECUTABLE=pip
+fi
 
-docker pull boundlessgeo/qgis-testing-environment:$QGIS_VERSION_TAG
+docker pull boundlessgeo/qgis-testing-environment:$TARGET_VERSION
+docker tag boundlessgeo/qgis-testing-environment:$TARGET_VERSION qgis-testing-environment
 
-docker run -d  --name qgis-testing-environment  -e DISPLAY=:99 -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/tests_directory boundlessgeo/qgis-testing-environment:$QGIS_VERSION_TAG
-
-
-docker exec -it qgis-testing-environment sh -c "qgis_setup.sh $PLUGIN_NAME"
+docker run -d  --name qgis-testing-environment  -e DISPLAY=:99 -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/tests_directory qgis-testing-environment
 
 # SSH key
 docker exec -it qgis-testing-environment sh -c "mkdir -p /root/.ssh"
@@ -27,10 +30,12 @@ docker exec -it qgis-testing-environment sh -c "printf 'Host *\n\tStrictHostKeyC
 docker exec -it qgis-testing-environment sh -c "cp /tests_directory/travis_setup/id_rsa /root/.ssh/id_rsa"
 docker exec -it qgis-testing-environment sh -c "chmod 600 /root/.ssh/id_rsa"
 
-# Install the plugin
-docker exec -it qgis-testing-environment sh -c "pip install paver"
+# Setup
+docker exec -it qgis-testing-environment sh -c "qgis_setup.sh $PLUGIN_NAME"
+docker exec -it qgis-testing-environment sh -c "$PIP_EXECUTABLE install paver"
+
+# Package
 docker exec -it qgis-testing-environment sh -c "cd /tests_directory && paver setup && paver package --tests"
-docker exec -it qgis-testing-environment sh -c "ln -s /tests_directory/$PLUGIN_NAME /root/.qgis2/python/plugins/$PLUGIN_NAME"
 
 # run the tests
 docker exec -it qgis-testing-environment sh -c "qgis_testrunner.sh webappbuilder.tests.layerstest.run_tests"
