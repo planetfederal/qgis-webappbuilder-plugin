3775b0cad37a3bbc40fa021bc59575dee8d7b8ad
Alexander Bruy <alexander.bruy@gmail.com>
2016-12-13 17:14:15 +0200

    use pip to install dependencies

--- pavement.py
+++ pavement.py
@@ -52,12 +52,20 @@ def setup(options):
         ext_libs.rmtree()
     ext_libs.makedirs()
     runtime, test = read_requirements()
+
+    try:
+        import pip
+    except:
+        error('FATAL: Unable to import pip, please install it first!')
+        sys.exit(1)
+
     os.environ['PYTHONPATH']=ext_libs.abspath()
     for req in runtime + test:
-        sh('easy_install -a -d %(ext_libs)s %(dep)s' % {
-            'ext_libs' : ext_libs.abspath(),
-            'dep' : req
-        })
+        pip.main(['install',
+                  '-t',
+                  ext_libs.abspath(),
+                  req])
+
     sdkPath = os.path.abspath("./webappbuilder/websdk")
 
     if os.path.exists(sdkPath):
