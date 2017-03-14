28571936a41053f3ecce8fb41701b792f3d6ec3f
volaya <volayaf@gmail.com>
2017-03-14 11:09:13 +0100

    Added zoom parameter to geolocation widget

--- webappbuilder/widgets/geolocation/geolocation.py
+++ webappbuilder/widgets/geolocation/geolocation.py
@@ -3,12 +3,21 @@ import os
 from PyQt4.QtGui import QIcon
 
 class Geolocation(WebAppWidget):
-
+    
+    levels = ["Current zoom"]
+    levels.extend((str(s) for s in range(1,25)))
+    _parameters = {"zoom": ("Current zoom", tuple(levels))}
+    
     def write(self, appdef, folder, app, progress):
+        try:
+            zoom = "zoom: %i" % int(self._parameters["zoom"])
+        except:
+            zoom =""
         app.panels.append('''React.createElement("div", {id:'geolocation-control'},
-                                    React.createElement(Geolocation, {map:map})
-                                  )''')
+                                    React.createElement(Geolocation, {map:map%s})
+                                  )''' % zoom)
         self.addReactComponent(app, "Geolocation")
+        
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(__file__), "geolocation.png"))
 
