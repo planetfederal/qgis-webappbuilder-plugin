41683408f7807a3ae145a08b8ebcdbfa0d5838ab
volaya <volayaf@gmail.com>
2017-03-22 17:56:13 +0100

    fixed test

--- webappbuilder/tests/expected/nochartwidget.js
+++ webappbuilder/tests/expected/nochartwidget.js
@@ -105,7 +105,8 @@ var BasicApp = React.createClass({
   _toggleEdit: function() {
     this._toggle(document.getElementById('edit-tool-panel'));
   },
-  _toggleAboutPanel: function() {
+  _toggleAboutPanel: function(evt) {
+    evt.preventDefault();
     this._toggle(document.getElementById('about-panel'));
   },
   _toggleChartPanel: function(evt) {
