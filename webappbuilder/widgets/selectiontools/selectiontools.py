48c2a9d9ba7b21bd0f83975e714d1dd6ec5d1091
volaya <volayaf@gmail.com>
2017-03-22 11:55:30 +0100

    ammend last commit

--- webappbuilder/widgets/selectiontools/selectiontools.py
+++ webappbuilder/widgets/selectiontools/selectiontools.py
@@ -6,12 +6,12 @@ class SelectionTools(WebAppWidget):
 
     def write(self, appdef, folder, app, progress):
         app.tools.append('''React.createElement(Select, {toggleGroup: 'navigation', map:map})''')
+        self.addReactComponent(app, "Select")
+
         nav = '''React.createElement(Navigation, {toggleGroup: 'navigation', secondary: true})'''
         if nav not in app.tools:
             app.tools.append(nav)
-
-        self.addReactComponent(app, "Select")
-        self.addReactComponent(app, "Navigation")
+            self.addReactComponent(app, "Navigation")
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(__file__), "selection-tool.png"))
