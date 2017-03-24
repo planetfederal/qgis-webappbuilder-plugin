9932223e8d1a7be8a6ca3932d427e18bff482123
volaya <volayaf@gmail.com>
2017-03-24 08:40:53 +0100

    added showEditForm parameter to WFST component

--- webappbuilder/widgets/wfst/wfst.py
+++ webappbuilder/widgets/wfst/wfst.py
@@ -4,11 +4,13 @@ from PyQt4.QtGui import QIcon
 
 class Wfst(WebAppWidget):
 
+    _parameters = {"showEditForm": False}
+
     def write(self, appdef, folder, app, progress):
         app.tools.append("React.createElement(Button, {label: 'WFS-T', onTouchTap: this._toggleWFST.bind(this)})")
         app.panels.append(''' React.createElement("div", {id: 'wfst', ref: 'wfstPanel'},
-                                      React.createElement(WFST, {map: map})
-                                    )''')
+                                      React.createElement(WFST, {showEditForm: %s, map: map})
+                                    )''' % str(self._parameters["showEditForm"]).lower())
         self.addReactComponent(app, "WFST")
 
     def icon(self):
