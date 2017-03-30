be8c4b6670a4d6dcbc24a3c30cd128cd66b412b7
bartvde <bartvde@boundlessgeo.com>
2017-03-30 21:24:08 +0200

    Add hideAboutPanel to app.jsx

--- webappbuilder/themes/basic/app.jsx
+++ webappbuilder/themes/basic/app.jsx
@@ -76,10 +76,10 @@ class BasicApp extends React.Component {
   _toggleEdit() {
     this._toggle(document.getElementById('edit-tool-panel'));
   }
-  _toggleAboutPanel(evt) {
+  _hideAboutPanel: function(evt) {
     evt.preventDefault();
-    this._toggle(document.getElementById('about-panel'));
-  }
+    document.getElementById('about-panel').style.display = 'none';
+  },
   _toggleChartPanel(evt) {
     evt.preventDefault();
     this._toggle(document.getElementById('chart-panel'));
