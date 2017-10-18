abd066bb29701c0651c8cfb768d83233261d6387
volaya <volayaf@gmail.com>
2017-10-18 11:04:14 +0200

    warning message if connect not available

--- webappbuilder/maindialog.py
+++ webappbuilder/maindialog.py
@@ -564,6 +564,15 @@ class MainDialog(BASE, WIDGET):
             appdef = self.createAppDefinition()
             problems = checkAppCanBeCreated(appdef)
             if pluginSetting("compileinserver"):
+                try:
+                    from boundlessconnect import connect
+                except ImportError:
+                    QMessageBox.warning(self, "Cannot compile in server",
+                                        "To compile the WebApp in the server, the Connect plugin is needed\n."
+                                        "Connect plugin could not be found. Install it or disable the 'Use SDK Service to compile app' option in the plugin settings",
+                                        QMessageBox.Close)
+                    return
+
                 QApplication.setOverrideCursor(QCursor(Qt.WaitCursor))
                 try:
                     checkSDKServerVersion()
